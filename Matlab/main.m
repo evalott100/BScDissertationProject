a4085288869570ae347adf6c55ff04b7e41e35db
Eva Lott <ELott@dundee.ac.uk>
2019-11-03 17:03:35 +0000

    Working on analytic matrix exponential form and verifying with matlab as I go

--- Matlab/main.m
+++ Matlab/main.m
@@ -1,4 +1,5 @@
-clc 
+clc
+clear
 
 
 % https://uk.mathworks.com/help/symbolic/solve-a-system-of-differential-equations.html
@@ -7,9 +8,10 @@ L = [1 2; -1 1];
 w = [x; y];
 odeSystem = diff(w) == L*w;
 sols = dsolve(odeSystem);
-sols.x
-sols.y
+[sols.x, sols.y]
+
 
 % Our solution with matrix exponential
-expform = simplify(rewrite(expm(L*t), 'sincos'))
-diff(w) == expform
+syms C3 C4
+expform = simplify(rewrite(expm(L*t), 'sincos'))*[C3; C4];
+[expform(1), expform(2)]
